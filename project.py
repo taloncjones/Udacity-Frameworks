5dbcc39bf9e4ba61b13be3d1a15e2e9677d58d4e
Talon Jones <taloncjones@gmail.com>
2019-02-24 21:15:37 +0000

    Sync project.py with course template

--- project.py
+++ project.py
@@ -1,10 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
@@ -12,6 +11,8 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
+# Output menu of restaurant specified by /restaurant/<id>/ path
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
@@ -24,6 +25,24 @@ def restaurantMenu(restaurant_id):
         output += "%s<br>" % i.description
         output += "<br>"
     return output
+
+# Task 1: Create route for newMenuItem function here
+
+def newMenuItem(restaurant_id):
+    return "Page to create a new menu item. Task 1 complete!"
+
+
+# Task 2: Create route for editMenuItem function here
+
+def editMenuItem(restaurant_id, menu_id):
+    return "Page to edit a menu item. Task 2 complete!"
+
+
+# Task 3: Create route for deleteMenuItem function here
+
+def deleteMenuItem(restaruant_id, menu_id):
+    return "Page to delete a menu item. Task 3 complete!"
+
 
 if __name__ == '__main__':
     app.debug = True
