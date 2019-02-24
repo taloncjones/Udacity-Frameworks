df972e7c56ac6b01d3520b44b6e43a26bae246ff
Talon Jones <taloncjones@gmail.com>
2019-02-24 23:30:33 +0000

    Add app.secret_key

--- project.py
+++ project.py
@@ -62,5 +62,6 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 8080)
