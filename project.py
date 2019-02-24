1b0bd549f114effebd4613c66c143227ab905c9d
Talon Jones <taloncjones@gmail.com>
2019-02-24 23:57:26 +0000

    Import jsonify

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
