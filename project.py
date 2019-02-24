046c0f4ba05c98c37010ffc49df4b46c83592d79
Talon Jones <taloncjones@gmail.com>
2019-02-24 23:29:27 +0000

    Import Flask flash

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
