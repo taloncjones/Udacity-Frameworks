021a21a42680697f42278b3e725685e509a79494
Talon Jones <taloncjones@gmail.com>
2019-02-25 21:18:23 +0000

    Add routes for site paths

--- finalproject.py
+++ finalproject.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+# finalproject.py for Udacity Course 3 created by Talon Jones
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,6 +10,7 @@ from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
 
 
+# Setup and bind db session
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
@@ -15,6 +18,34 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+### Routes for finalproject.py go here
+# Routes for main menu (list of restaurants)
+@app.route('/')
+@app.route('/restaurants/')
+
+# Route for adding a new restaurant
+@app.route('/restaurants/new/')
+
+# Route for editing a restaurant
+@app.route('/restaurants/<int:rest_id>/edit/')
+
+# Route for deleting a restaurant
+@app.route('/restaurants/<int:rest_id>/delete/')
+
+# Routes for restaurant menu specified by <rest_id>
+@app.route('/restaurants/<int:rest_id>/')
+@app.route('/restaurants/<int:rest_id>/menu/')
+
+# Route for adding a new restaurant menu item
+@app.route('/restaurants/<int:rest_id>/new/')
+
+# Route for editing a restaurant menu item
+@app.route('/restaurants/<int:rest_id>/<int:item_id>/edit/')
+
+# Route for deleting a restaurant menu item
+@app.route('/restaurants/<int:rest_id>/<int:item_id>/delete/')
+
+# Run if called by file finalproject.py
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 8000)
