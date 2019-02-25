146eefc76300ed6ac56c537e06830b7d75261a2c
Talon Jones <taloncjones@gmail.com>
2019-02-25 20:58:47 +0000

    Import dependencies

--- finalproject.py
+++ finalproject.py
@@ -1,7 +1,13 @@
-from flask import flask
+#!/usr/bin/env python
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+
 app = Flask(__name__)
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 8080)
+    app.run(host = '0.0.0.0', port = 8000)
