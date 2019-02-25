9e1d0f5fc31b689560f94892da20dc9089d2fd37
Talon Jones <taloncjones@gmail.com>
2019-02-25 21:00:50 +0000

    Create and bind database session

--- finalproject.py
+++ finalproject.py
@@ -8,6 +8,13 @@ from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
 
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 8000)
