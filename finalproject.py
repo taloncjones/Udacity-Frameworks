b29edd3e420637dd22109f7ed708a927c6a5c1a5
Talon Jones <taloncjones@gmail.com>
2019-02-25 21:35:22 +0000

    Add basic text returns for routes

--- finalproject.py
+++ finalproject.py
@@ -22,28 +22,44 @@ session = DBSession()
 # Routes for main menu (list of restaurants)
 @app.route('/')
 @app.route('/restaurants/')
+def allRestaurants():
+    return "List of restaurants"
 
 # Route for adding a new restaurant
 @app.route('/restaurants/new/')
+def newRestaurant():
+    return "New restaurant"
 
 # Route for editing a restaurant
 @app.route('/restaurants/<int:rest_id>/edit/')
+def editRestaurant(rest_id):
+    return "Edit restaurant %s" % rest_id
 
 # Route for deleting a restaurant
 @app.route('/restaurants/<int:rest_id>/delete/')
+def deleteRestaurant(rest_id):
+    return "Delete restaurant %s" % rest_id
 
 # Routes for restaurant menu specified by <rest_id>
 @app.route('/restaurants/<int:rest_id>/')
 @app.route('/restaurants/<int:rest_id>/menu/')
+def restaurantMenu(rest_id):
+    return "Menu for restaurant %s" % rest_id
 
 # Route for adding a new restaurant menu item
 @app.route('/restaurants/<int:rest_id>/new/')
+def newMenuItem(rest_id):
+    return "New menu item for restaurant %s" % rest_id
 
 # Route for editing a restaurant menu item
 @app.route('/restaurants/<int:rest_id>/<int:item_id>/edit/')
+def editMenuItem(rest_id, item_id):
+    return "Edit menu item %s" % item_id
 
 # Route for deleting a restaurant menu item
 @app.route('/restaurants/<int:rest_id>/<int:item_id>/delete/')
+def deleteMenuItem(rest_id, item_id):
+    return "Delete menu item %s" % item_id
 
 # Run if called by file finalproject.py
 if __name__ == '__main__':
